a02b565f7d77d6e9640b441999825bc949b35edc
Jeff Nelson <jtnelson1@gmail.com>
2014-01-31 20:07:07 -0500

    initial commit
    Change-Id: If3029fd6faa23dc2d209896211d2ce8d33e91022

---
+++ src/main/java/org/cinchapi/concourse/importer/ImportResult.java
@@ -0,0 +1,129 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse.importer;
+
+import java.util.List;
+import java.util.Set;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
+
+/**
+ * An ImportResult contains information about the nature of an attempt to import
+ * data into one or more records (i.e. did the import succeed, etc).
+ *
+ * @author jnelson
+ */
+public final class ImportResult {
+
+    // NOTE: This class does not define hashCode(), equals() or toString()
+    // because the defaults are the desired behaviour
+
+    /**
+     * Return an {@link ImportResult} that describes what occurs during an
+     * attempt to import {@code importData} into {@code record}.
+     *
+     * @param importData
+     * @param records
+     * @return the ImportRecordResult
+     */
+    public static ImportResult newImportResult(
+            Multimap<String, String> importData, Set<Long> records) {
+        return new ImportResult(importData, records);
+    }
+
+    /**
+     * A collection of strings describing errors that occurred during the
+     * import.
+     */
+    private final List<String> errors = Lists.newArrayList();
+
+    /**
+     * The raw data that was used in the attempted import.
+     */
+    private final Multimap<String, String> importData;
+
+    /**
+     * The records into which the data was imported.
+     */
+    private final Set<Long> records;
+
+    /**
+     * Construct a new instance.
+     *
+     * @param importData
+     */
+    private ImportResult(Multimap<String, String> importData, Set<Long> records) {
+        this.importData = importData;
+        this.records = records;
+    }
+
+    /**
+     * Add an indication that an error has occurred, described by
+     * {@code message}.
+     *
+     * @param message
+     */
+    protected void addError(String message) {
+        errors.add(message);
+    }
+
+    /**
+     * Return the number of errors that occurred during the import.
+     *
+     * @return the error count
+     */
+    public int getErrorCount() {
+        return errors.size();
+    }
+
+    /**
+     * Return the raw data that was used in the import.
+     *
+     * @return the import data
+     */
+    public Multimap<String, String> getImportData() {
+        return importData;
+    }
+
+    /**
+     * Return the records into which the data was imported.
+     *
+     * @return the records
+     */
+    public Set<Long> getRecords() {
+        return records;
+    }
+
+    /**
+     * Return {@code true} if the import encountered errors.
+     *
+     * @return {@code true} if there were errors
+     */
+    public boolean hasErrors() {
+        return !errors.isEmpty();
+    }
+
+}
+
---
+++ src/main/java/org/cinchapi/concourse/importer/LineImporter.java
@@ -0,0 +1,166 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jeff Nelson, Cinchapi Software Collective
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.cinchapi.concourse.importer;
+
+import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.Set;
+
+import javax.annotation.Nullable;
+
+import org.cinchapi.concourse.Link;
+import org.cinchapi.concourse.thrift.Operator;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+
+/**
+ * A {@link LineImporter} is one that handles files where each line represents a
+ * single collection of data that should be imported into one or more (possibly
+ * existing) records.
+ * 
+ * @author jnelson
+ */
+public abstract class LineImporter extends AbstractImporter {
+
+    @Override
+    public final void importFile(String file) {
+        importFile(file, null); // for the default cause,
+                                // assume that we want to
+                                // import each line into a
+                                // new record
+    }
+
+    /**
+     * Import the data in {@code file}.
+     * <p>
+     * Each individual line of the file will be possibly split by some
+     * delimiter, processed as a collection and added to one or more records in
+     * {@link Concourse}.
+     * </p>
+     * <p>
+     * <strong>Note</strong> that if {@code resolveKey} is specified, an attempt
+     * will be made to add the data in each line into the existing records that
+     * are found using {@code resolveKey} and its corresponding value in the
+     * line.
+     * </p>
+     * 
+     * @param file
+     * @param resolveKey
+     */
+    public abstract void importFile(String file, @Nullable String resolveKey); // subclass
+                                                                               // should
+                                                                               // define
+                                                                               // its
+                                                                               // own
+                                                                               // delimiter
+
+    /**
+     * Import a single line that has been transformed into {@code data}.
+     * <p>
+     * If {@code resolveKey} is specified, it is possible that the {@code data}
+     * will be added to more than one existing record. It is guaranteed that an
+     * attempt will be made to add the data to at least one (possibly) new
+     * record.
+     * </p>
+     * 
+     * @param data
+     * @param resolveKey
+     * @return an {@link ImportResult} object that describes the records
+     *         created/affected from the import and whether any errors occurred.
+     */
+    protected final ImportResult importLine(Multimap<String, String> data,
+            @Nullable String resolveKey) {
+        // Attempt to resolve the data into one or more existing records,
+        // otherwise create a new record
+        Collection<String> resolveValues;
+        String resolveValue;
+        Set<Long> records;
+        if(!Strings.isNullOrEmpty(resolveKey)
+                && (resolveValues = data.get(resolveKey)).size() == 1
+                && Strings.isNullOrEmpty((resolveValue = resolveValues
+                        .iterator().next()))) {
+            records = concourse.find(resolveKey, Operator.EQUALS, resolveValue);
+        }
+        else {
+            records = Sets.newHashSet(concourse.create());
+        }
+        // Iterate through the data and add it to Concourse
+        ImportResult result = ImportResult.newImportResult(data, records);
+        for (String key : data.keySet()) {
+            for (String rawValue : data.get(key)) {
+                Object value = convert(rawValue);
+                for (long record : records) {
+                    if(!concourse.add(key, value, record)) {
+                        result.addError(MessageFormat.format(
+                                "Could not import {0} AS {1} IN {2}", key,
+                                value, record));
+                    }
+                }
+            }
+        }
+        return result;
+
+    }
+
+    /**
+     * Analyze {@code value} and convert it to the appropriate Java primitive or
+     * Object
+     * 
+     * @param value
+     * @return the converted value
+     */
+    protected final Object convert(String value) { // visible for testing
+        if(value.matches("\"([^\"]+)\"|'([^']+)'")) {
+            return value.substring(1, value.length() - 1);
+        }
+        else if(value.matches("@[0-9]+@")) {
+            return Link.to(Long.parseLong(value.replace("@", "")));
+        }
+        else if(value.equalsIgnoreCase("true")) {
+            return true;
+        }
+        else if(value.equalsIgnoreCase("false")) {
+            return false;
+        }
+        else {
+            Class<?>[] classes = { Integer.class, Long.class, Float.class,
+                    Double.class };
+            for (Class<?> clazz : classes) {
+                try {
+                    return clazz.getMethod("valueOf", String.class).invoke(
+                            null, value);
+                }
+                catch (Exception e) {
+                    if(e instanceof NumberFormatException
+                            || e.getCause() instanceof NumberFormatException) {
+                        continue;
+                    }
+                }
+            }
+            return value;
+        }
+    }
+}
