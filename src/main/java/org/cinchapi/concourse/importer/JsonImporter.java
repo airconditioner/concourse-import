cbd77716630139f7549f83f89d8d188b55921aa1
henry <henry.yin@uq.net.au>
2015-03-22 19:06:17 +1000

    formatting update

--- src/main/java/org/cinchapi/concourse/importer/JsonImporter.java
+++ src/main/java/org/cinchapi/concourse/importer/JsonImporter.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  * 
- * Copyright (c) 2014 Jeff Nelson, Cinchapi Software Collective
+ * Copyright (c) 2015 Jeff Nelson, Cinchapi Software Collective
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -34,75 +34,79 @@ import org.cinchapi.concourse.util.FileUtility;
 
 import com.google.common.base.Throwables;
 
-
 /**
  * <p>
- * An {@link Importer} that can handle files in Json syntax.
- * It is assumed in this Json format that single quotes are not accepted for 
- * enclosing keys or values, but they can be used as characters in a string. 
+ * An {@link Importer} that can handle files in Json syntax. It is assumed in
+ * this Json format that single quotes are not accepted for enclosing keys or
+ * values, but they can be used as characters in a string.
  * </p>
+ * 
  * @author hyin
  */
 public class JsonImporter extends AbstractImporter {
-	
+
     /**
      * Construct a new instance with a Concourse instance.
      * 
      * @param concourse
      */
-	protected JsonImporter(Concourse concourse) {
-		super(concourse);
-	}
+    protected JsonImporter(Concourse concourse) {
+        super(concourse);
+    }
 
-	@Override
-	/**
-	 *  Import the Json objects by parsing each Json object in
-	 *  the file and calling the Concourse insert method on it.
-	 * 
-	 *  @param file
-	 *  @param resolveKey
-	 *  @return the results of the import
-	 */
-	protected Collection<ImportResult> handleFileImport(String file,
-			@Nullable String resolveKey) {
-		try {
-			FileReader fr = new FileReader(FileUtility.expandPath(file));
-			BufferedReader reader = new BufferedReader(fr);
-			
-			StringBuilder jsonObj= new StringBuilder();
-			
-			// Boolean tags to indicate start of
-			// Json object and String
-	        boolean start = false;
-	        boolean quote = false;
-	        char doubleQuote = '\"';
-	        int c;
-			while ((c = reader.read()) != -1) {
-				c = (char) c;
-	            if (c == doubleQuote && start == true) {
-	                quote = !quote;
-	                jsonObj.append(c); 
-	            }
-	            else if (c == '{' && start == false && !quote) {
-	                start = true;
-	                jsonObj.append(c);
-	            }
-	            else if (c == '}' && start == true && !quote) {
-	                start = false;
-	                jsonObj.append(c);
-	                concourse.insert((jsonObj.toString()));
-	                jsonObj = new StringBuilder();
-	            }
-	            else if (start == true) {
-	            	jsonObj.append(c);
-	            }
-			}
-			reader.close();
-		} 
-		catch (Exception e) {
-			throw Throwables.propagate(e);
-		}
-		return null;
-	}
+    @Override
+    /**
+     *  Import the Json objects by parsing each Json object in
+     *  the file and calling the Concourse insert method on it.
+     * 
+     *  @param file
+     *  @param resolveKey
+     *  @return the results of the import
+     */
+    protected Collection<ImportResult> handleFileImport(String file,
+            @Nullable String resolveKey) {
+        try {
+            FileReader fr = new FileReader(FileUtility.expandPath(file));
+            BufferedReader reader = new BufferedReader(fr);
+
+            StringBuilder jsonObj = new StringBuilder();
+
+            // Boolean tags to indicate start of
+            // Json object and String
+            boolean start = false;
+            boolean quote = false;
+            char doubleQuote = '\"';
+            
+            int c;
+            while ((c = reader.read()) != -1) {
+                c = (char) c;
+                if (c == doubleQuote && start == true) {
+                    quote = !quote;
+                    jsonObj.append(c);
+                }
+                // Brackets in double quotes should count as 
+                // character in String, not start/end of object
+                else if (c == '{' && start == false && !quote) {
+                    start = true;
+                    jsonObj.append(c);
+                } 
+                else if (c == '}' && start == true && !quote) {
+                    start = false;
+                    jsonObj.append(c);
+                    concourse.insert((jsonObj.toString()));
+                    jsonObj = new StringBuilder();
+                } 
+                else if (start == true) {
+                    jsonObj.append(c);
+                }
+            }
+            reader.close();
+            
+        }
+        catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+        return null;
+    }
 
 }
